000d3f636df979dfd60903a648181115739da4aa
hiso0ka44 <princeofshadows1@gmail.com>
2024-10-01 17:06:50 +0300

    made cubit and states

---
+++ lib/cubit/couter_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:basketball_counter/cubit/counter_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CouterCubit  extends Cubit<CounterStates>
+{
+  CouterCubit(super.initialState);
+    
+}
